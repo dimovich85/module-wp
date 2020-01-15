c8e96b9c173b26926167942669310fc2fd8c895d
dimovich85 <dimovich85@gmail.com>
2020-01-15 20:25:25 +0200

    Сделал вывод меню. Поправил CSS.

--- 404.php
+++ 404.php
@@ -2,14 +2,10 @@
     /* Template Name: Шаблон для главной */
     get_header(); 
 ?>
-    <main class="main-content" style="background-attachment: fixed;">
-        <h1 
-            class="main-heading main-heading_404"
-            style="padding-top: 200px; padding-bottom: 200px; text-align: center; font-size: 6vw;"
-        > 
+    <main class="main-content">
+        <h1 class="main-heading main-heading_404"> 
             404 
         </h1>
-        <!-- TODO: добавить в css -->
     </main>
 
 <?php get_footer(); ?>
