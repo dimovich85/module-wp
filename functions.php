5c8fb738016bcaadcc87ea14f97e88628e35e2b1
dimovich85 <dimovich85@gmail.com>
2020-01-15 16:28:06 +0200

    Зарегистрировал зоны сайдбаров для виджетов

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 add_action('wp_enqueue_scripts', 'sport_scripts');
 add_action('after_setup_theme', 'sport_setup');
-//add_action('widgets_init', 'sport_widgets');
+add_action('widgets_init', 'sport_widgets');
 //add_action('init', 'sport_registration');
 
 add_filter('show_admin_bar', '__return_false');
@@ -20,6 +20,88 @@ function sport_setup(){
     add_theme_support('custom-logo');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+}
+
+function sport_widgets(){
+    register_sidebar([
+        'name' => 'Шапка',
+        'id' => 'header',
+        'description' => 'Сайдбар в шапке для контактов',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Главная страница - Блок Кто мы',
+        'id' => 'main_about_us',
+        'description' => 'Блок "Кто мы" на главной странице',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Подвал - Контакты',
+        'id' => 'footer',
+        'description' => 'Зона в подвале, для виджетов с контактами, которая дублрует шапку',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Футер - Колонка 1',
+        'id' => 'footer_column1',
+        'description' => 'Первая колонка в футере. Где выводится копирайт',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Футер - колонка 2',
+        'id' => 'footer_column2',
+        'description' => 'Вторая колонка в футере. Где выводится текст для обратной связи',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Футер - Колонка 3',
+        'id' => 'footer_column3',
+        'description' => 'Третья колонка в футере. Где выводятся ссылки на социальные сети',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Модальное окно - Записаться',
+        'id' => 'modal_subscribe',
+        'description' => 'Зона в модальном окне для формы ОС. Выводится в первую очередь на странице с Услугами',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Контакты - Карта',
+        'id' => 'contacts_map',
+        'description' => 'Зона на странице с контактами, куда выводится карта',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Контакты - Карта',
+        'id' => 'contacts_map',
+        'description' => 'Зона на странице с контактами, куда выводится карта',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
+
+    register_sidebar([
+        'name' => 'Контакты - виджеты под картой',
+        'id' => 'contacts_widgets',
+        'description' => 'Зона на странице с контактами, куда выводится мелкие виджеты с информацией',
+        'before_widget' => null,
+        'after_widget' => null
+    ]);
 }
 
 function _img_url($path){
