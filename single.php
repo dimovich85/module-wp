0ce28d985e5e2509183249ea6c7e73913146ae9d
dimovich85 <dimovich85@gmail.com>
2020-01-17 18:42:15 +0200

    Динамика для шаблона 1ой записи

--- single.php
+++ single.php
@@ -6,21 +6,33 @@
       get_template_part('tmp/breadcrumbs'); 
     ?>
     </div>
+    <?php
+      if( have_posts() ):
+        while( have_posts() ): the_post();
+    ?>
     <article class="main-article wrapper">
       <header class="main-article__header">
-        <img src="<?php _img_url('img/blog__article_full.jpg'); ?>" alt="" class="main-article__thumb">
-        <h1 class="main-article__h">Рельефный пресс</h1>
+        <?php the_post_thumbnail( 'full', ['class' => 'main-article__thumb'] ); ?>
+        <h1 class="main-article__h">
+          <?php the_title(); ?>
+        </h1>
       </header>
-      <p> Таким образом новая модель организационной деятельности позволяет оценить значение системы обучения кадров, соответствует насущным потребностям. Идейные соображения высшего порядка, а также постоянный количественный рост и сфера нашей активности обеспечивает широкому кругу (специалистов) участие в формировании модели развития. </p>
-      <p> Значимость этих проблем настолько очевидна, что новая модель организационной деятельности в значительной степени обуславливает создание модели развития. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности способствует подготовки и реализации систем массового участия. Идейные соображения высшего порядка, а также дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. </p>
+      <?php the_content(); ?>
       <footer class="main-article__footer">
-        <time datetime="01-01-2020">1 января</time>
+        <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+          <?php echo get_the_date('d F Y'); ?>
+        </time>
         <a href="#" class="main-article__like like">
           <span class="like__text">Нравится </span>
           <span class="like__count">46</span>
         </a>
       </footer>
     </article>
+    <?php
+        endwhile;
+      else: get_template_part( 'tmp/no_posts');
+    endif;
+    ?>
   </main>
 
 <?php get_footer(); ?>
