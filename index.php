767ab3d2ec04dce229cf6dc774e9670cf3335160
dimovich85 <dimovich85@gmail.com>
2020-01-17 16:52:18 +0200

    Шаблон блога и дефольный вывод постов - index.php готов

--- index.php
+++ index.php
@@ -3,93 +3,123 @@
   if( is_home() ):
 ?>
   <main class="main-content">
-      <h1 class="sr-only">Страница категорий блога на сайте спорт-клуба SportIsland</h1>
+    <h1 class="sr-only">Страница категорий блога на сайте спорт-клуба SportIsland</h1>
+    <div class="wrapper">
+    <?php 
+      get_template_part('tmp/breadcrumbs');
+    ?>
+    </div>
+    <?php if( have_posts() ): ?>
+    <section class="last-posts">
       <div class="wrapper">
-      <?php 
-        get_template_part('tmp/breadcrumbs'); 
-      ?>
+        <h2 class="main-heading last-posts__h"> последние записи </h2>
+        <ul class="posts-list">
+        <?php 
+          while( have_posts() ): the_post(); 
+        ?>
+          <li class="last-post">
+            <a href="<?php the_permalink(); ?>" class="last-post__link" aria-label="Читать текст статьи: <?php the_title(); ?>">
+              <figure class="last-post__thumb">
+                <?php the_post_thumbnail( 'full', ['class' => 'last-post__img'] ); ?>
+              </figure>
+              <div class="last-post__wrap">
+                <h3 class="last-post__h"> <?php the_title(); ?> </h3>
+                <p class="last-post__text">
+                  <?php
+                    echo mb_substr(strip_tags(get_the_content()), 0, 200) . '...';
+                  ?>
+                </p>
+                <span class="last-post__more link-more">Подробнее</span>
+              </div>
+            </a>
+          </li>
+        <?php endwhile; ?>
+        </ul>
       </div>
-      <section class="last-posts">
-        <div class="wrapper">
-          <h2 class="main-heading last-posts__h"> последние записи </h2>
-          <ul class="posts-list">
-            <li class="last-post">
-              <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Растяжка от болей в мышцах">
-                <figure class="last-post__thumb">
-                  <img src="<?php _img_url('img/blog__article_thmb1.jpg'); ?>" alt="" class="last-post__img">
-                </figure>
-                <div class="last-post__wrap">
-                  <h3 class="last-post__h"> Растяжка от болей в мышцах </h3>
-                  <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-                  <span class="last-post__more link-more">Подробнее</span>
-                </div>
-              </a>
-            </li>
-            <li class="last-post">
-              <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Бег помогает похудеть">
-                <figure class="last-post__thumb">
-                  <img src="<?php _img_url('img/blog__article_thmb2.jpg'); ?>" alt="" class="last-post__img">
-                </figure>
-                <div class="last-post__wrap">
-                  <h3 class="last-post__h"> Бег помогает похудеть </h3>
-                  <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-                  <span class="last-post__more link-more">Подробнее</span>
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-      <section class="categories">
-        <div class="wrapper">
-          <h2 class="categories__h main-heading"> категории </h2>
-          <ul class="categories-list">
-            <li class="category">
-              <a href="category.html" class="category__link">
-                <img src="<?php _img_url('img/blog__category_thmb1.jpg'); ?>" alt="" class="category__thumb">
-                <span class="category__name">Груповые занятия</span>
-              </a>
-            </li>
-            <li class="category">
-              <a href="category.html" class="category__link">
-                <img src="<?php _img_url('img/blog__category_thmb2.jpg'); ?>" alt="" class="category__thumb">
-                <span class="category__name">Кардио</span>
-              </a>
-            </li>
-            <li class="category">
-              <a href="category.html" class="category__link">
-                <img src="<?php _img_url('img/blog__category_thmb3.jpg'); ?>" alt="" class="category__thumb">
-                <span class="category__name">Йога</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+    </section>
+    <?php
+      else:
+        get_template_part( 'tmp/no_posts.php' );
+      endif;
+    ?>
+    <section class="categories">
+      <div class="wrapper">
+        <h2 class="categories__h main-heading"> категории </h2>
+        <?php
+          $cats = get_categories();
+          if( $cats ):
+        ?>
+        <ul class="categories-list">
+        <?php
+          foreach( $cats as $cat ):
+            $image = get_field( 'category_thumb', 'category_' . $cat->cat_ID );
+            $alt = $image['alt'];
+            $url = $image['url'];
+            $cat_link = get_category_link( $cat->cat_ID );
+        ?>
+          <li class="category">
+            <a href="<?php echo $cat_link; ?>" class="category__link">
+              <img 
+                src="<?php echo $img; ?>" 
+                alt="Категория <?php echo $cat->name; echo ' '.$alt; ?>" 
+                class="category__thumb"
+              >
+              <span class="category__name"><?php echo $cat->name; ?></span>
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+        <?php
+          else: 
+            get_template_part('tmp/no_posts');
+          endif;
+        ?>
+      </div>
+    </section>
   </main>
 <?php
   else:
 ?>
   <main class="main-content">
+    <h1 class="sr-only">Страница на сайте спорт-клуба SportIsland</h1>
     <div class="wrapper">
     <?php 
-      get_template_part('tmp/breadcrumbs'); 
+      get_template_part('tmp/breadcrumbs');
     ?>
     </div>
-    <article class="main-article wrapper">
-      <header class="main-article__header">
-        <img src="<?php _img_url('img/blog__article_full.jpg'); ?>" alt="" class="main-article__thumb">
-        <h1 class="main-article__h">Рельефный пресс</h1>
-      </header>
-      <p> Таким образом новая модель организационной деятельности позволяет оценить значение системы обучения кадров, соответствует насущным потребностям. Идейные соображения высшего порядка, а также постоянный количественный рост и сфера нашей активности обеспечивает широкому кругу (специалистов) участие в формировании модели развития. </p>
-      <p> Значимость этих проблем настолько очевидна, что новая модель организационной деятельности в значительной степени обуславливает создание модели развития. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности способствует подготовки и реализации систем массового участия. Идейные соображения высшего порядка, а также дальнейшее развитие различных форм деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. </p>
-      <footer class="main-article__footer">
-        <time datetime="01-01-2020">1 января</time>
-        <a href="#" class="main-article__like like">
-          <span class="like__text">Нравится </span>
-          <span class="like__count">46</span>
-        </a>
-      </footer>
-    </article>
+    <?php if( have_posts() ): ?>
+    <section class="last-posts">
+      <div class="wrapper">
+        <h2 class="main-heading last-posts__h"> Записи </h2>
+        <ul class="posts-list">
+        <?php 
+          while( have_posts() ): the_post(); 
+        ?>
+          <li class="last-post">
+            <a href="<?php the_permalink(); ?>" class="last-post__link" aria-label="Читать текст статьи: <?php the_title(); ?>">
+              <figure class="last-post__thumb">
+                <?php the_post_thumbnail( 'full', ['class' => 'last-post__img'] ); ?>
+              </figure>
+              <div class="last-post__wrap">
+                <h3 class="last-post__h"> <?php the_title(); ?> </h3>
+                <p class="last-post__text">
+                  <?php
+                    echo mb_substr(strip_tags(get_the_content()), 0, 200) . '...';
+                  ?>
+                </p>
+                <span class="last-post__more link-more">Подробнее</span>
+              </div>
+            </a>
+          </li>
+        <?php endwhile; ?>
+        </ul>
+      </div>
+    </section>
+    <?php
+      else:
+        get_template_part( 'tmp/no_posts.php' );
+      endif;
+    ?>
   </main>
 <?php 
   endif;
