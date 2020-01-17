f64c0562fae2372b87a0a59879cdfdd02fa08853
dimovich85 <dimovich85@gmail.com>
2020-01-17 14:59:14 +0200

    Убрал лишние комментарии

--- category.php
+++ category.php
@@ -40,52 +40,8 @@
               </a>
             </li>
           <?php endwhile; ?>
-            <!-- <li class="last-post">
-              <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Бег помогает похудеть">
-                <figure class="last-post__thumb">
-                  <img src="<?php _img_url('img/blog__article_thmb2.jpg'); ?>" alt="" class="last-post__img">
-                </figure>
-                <div class="last-post__wrap">
-                  <h3 class="last-post__h"> Бег помогает похудеть </h3>
-                  <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-                  <span class="last-post__more link-more">Подробнее</span>
-                </div>
-              </a>
-            </li>
-            <li class="last-post">
-              <a href="single.html" class="last-post__link" aria-label="Читать текст статьи: Рельефный пресс">
-                <figure class="last-post__thumb">
-                  <img src="<?php _img_url('img/blog__article_thmb3.jpg'); ?>" alt="" class="last-post__img">
-                </figure>
-                <div class="last-post__wrap">
-                  <h3 class="last-post__h"> Рельефный пресс </h3>
-                  <p class="last-post__text"> Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. </p>
-                  <span class="last-post__more link-more">Подробнее</span>
-                </div>
-              </a>
-            </li> -->
           </ul>
           <?php the_posts_pagination(); ?>
-          <!-- <nav class="pagination">
-            <h3 class="sr-only">Навигация</h3>
-            <ul class="pagination__links">
-              <li class="pagination__block pagination__block_current">
-                <a href="#" class="pagination__link"> 1 </a>
-              </li>
-              <li class="pagination__block">
-                <a href="#" class="pagination__link"> 2 </a>
-              </li>
-              <li class="pagination__block">
-                <a href="#" class="pagination__link"> 3 </a>
-              </li>
-              <li class="pagination__block">
-                <a href="#" class="pagination__link"> 4 </a>
-              </li>
-              <li class="pagination__block">
-                <a href="#" class="pagination__link"> 5 </a>
-              </li>
-            </ul>
-          </nav> -->
         </div>
       </section>
       <?php 
