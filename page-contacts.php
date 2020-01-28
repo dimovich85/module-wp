ea9cdd624c034d8ec262767bc102b467eec3e5c2
dimovich85 <dimovich85@gmail.com>
2020-01-28 17:40:41 +0200

    Сделал рабочую формы ОС. TODO верстка

--- page-contacts.php
+++ page-contacts.php
@@ -31,25 +31,7 @@
             ?>
           </p>
           <h2 class="page-heading contacts__h_form"> форма </h2>
-          <form action="#" class="contacts__form contacts-form">
-            <label class="contacts-form__label">
-              <span class="sr-only"> Имя </span>
-              <input type="text" class="contacts-form__input" placeholder="Ваше имя">
-            </label>
-            <label class="contacts-form__label">
-              <span class="sr-only"> Телефон </span>
-              <input type="text" class="contacts-form__input" placeholder="Ваш телефон">
-            </label>
-            <label class="contacts-form__label">
-              <span class="sr-only"> Почта </span>
-              <input type="text" class="contacts-form__input" placeholder="Ваша почта">
-            </label>
-            <label class="contacts-form__label contacts-form__label_textarea">
-              <span class="sr-only"> Комментарий </span>
-              <textarea name="" id="" cols="30" rows="10" class="contacts-form__textarea" placeholder="Ваше сообщение"></textarea>
-            </label>
-            <button class="contacts-form__btn btn"> Отправить </button>
-          </form>
+          <?php echo do_shortcode('[contact-form-7 id="5" html_class="contacts__form contacts-form"]'); ?>
         </div>
       </section>
     </main>
