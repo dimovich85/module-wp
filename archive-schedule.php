4079eb2ce843021a3f528e6944dfa318c5e150ca
dimovich85 <dimovich85@gmail.com>
2020-01-29 12:56:32 +0200

    Поправил вывод расписания

--- archive-schedule.php
+++ archive-schedule.php
@@ -38,6 +38,7 @@
             foreach( $days as $day ):
               $day_desc = $day->description;
               $content = new WP_Query([
+                'posts_per_page' => -1,
                 'post_type' => 'schedule',
                 'days' => $day->slug,
                 'meta_key' => 'schedule_time_start',
@@ -53,18 +54,18 @@
               <h2 class="sr-only"><?php echo $day->description; ?></h2>
               <ul class="schedule tabs-content__list">
               <?php
-                  foreach( $content->posts as $activity ):
-                    $data = get_fields( $activity->ID );
-                    $trainer = esc_html(get_the_title($data['schedule_trainer'][0]));
-                    $time = '';
-                    $start = $data['schedule_time_start'];
-                    $end = $data['schedule_time_end'];
-                    $time = $start . ' - ' . $end;
-                    $title = $activity->post_title;
-                    $place = get_the_terms($activity, 'places')[0];
-                    $place_name = $place->name;
-                    $place_color = get_field('places_color', 'places_'.$place->term_id);
-                ?>
+                foreach( $content->posts as $activity ):
+                  $data = get_fields( $activity->ID );
+                  $trainer = esc_html(get_the_title($data['schedule_trainer'][0]));
+                  $time = '';
+                  $start = $data['schedule_time_start'];
+                  $end = $data['schedule_time_end'];
+                  $time = $start . ' - ' . $end;
+                  $title = $activity->post_title;
+                  $place = get_the_terms($activity, 'places')[0];
+                  $place_name = $place->name;
+                  $place_color = get_field('places_color', 'places_'.$place->term_id);
+              ?>
                 <li class="schedule__item">
                   <p class="schedule__time"> <?php echo $time; ?> </p>
                   <h3 class="schedule__h"> <?php echo $title; ?> </h3>
@@ -76,7 +77,7 @@
                     <?php echo $place_name; ?> 
                   </p>
                 </li>
-                <?php endforeach; ?>
+              <?php endforeach; ?>
               </ul>
             </li>
           <?php $index++; endforeach; ?>
