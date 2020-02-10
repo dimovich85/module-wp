2584df3e887b9fda7b5cfa5aeb1a23935a359151
dimovich85 <dimovich85@gmail.com>
2020-02-10 19:05:34 +0200

    Исправленная верстка. Поправлены "хлебные крошки"

--- tmp/breadcrumbs.php
+++ tmp/breadcrumbs.php
@@ -1,16 +1,6 @@
-<ul class="breadcrumbs">
-<?php
-    $breadcrumbs = breadcrumbs();
-    $class_text = '';
-    foreach( $breadcrumbs as $ind => $item ):
-    if( $ind !== 0 ){
-        $class_text = ' breadcrumbs__item_home';
-    }
-?>
-    <li class="breadcrumbs__item<?php echo $classtext; ?>">
-        <a href="<?php echo $item['link']; ?>" class="breadcrumbs__link">
-            <?php echo $item['text']; ?>
-        </a>
-    </li>
-<?php endforeach; ?>
-</ul>
+<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <?php if(function_exists('bcn_display'))
+    {
+        bcn_display();
+    }?>
+</div>
